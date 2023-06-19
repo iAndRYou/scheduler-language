2476c2a35c1062a0b3c18420872738f366a92558
iAndRYou <nawet3razy@gmail.com>
2023-06-19 22:38:32 +0200

    powinno sie ustawiac tam gdzie trzeba

--- frontend/schedule.js
+++ frontend/schedule.js
@@ -1,10 +1,48 @@
+function loadjson() {
+    // output.json
+    fetch("http://[::]:9090/test.json").then(response =>
+        response.json().then(data => {
+            console.log(data);
+            for (var key in data) {
+                console.log(data[key]);
+                createClassContainer(data[key]);
+            }
+        }
+    ).catch(err => {
+        console.log(err);
+    }));
+}
+
 function createAbbreviation(subject) {
     var abbreviation = "";
-    var words = subject.oUpperCase().split(" ");
-    for (var word in words) {
-        abbreviation += word.charAt(0);
+    var words = subject.toUpperCase().split(" ");
+    for (var i = 0; i < words.length; i++) {
+        abbreviation += words[i].charAt(0);
     }
     return abbreviation;
+}
+
+function calculateHeight(start, end) {
+    var startParts = start.split(":");
+    var endParts = end.split(":");
+
+    var startMinutes = +startParts[0] * 60 + +startParts[1];
+    var endMinutes = +endParts[0] * 60 + +endParts[1];
+
+    var height = (endMinutes - startMinutes);
+    return height;
+}
+
+function calculateTop(start) {
+    var startParts = start.split(":");
+    var startMinutes = +startParts[0] * 62 + +startParts[1];
+
+    return startMinutes + 20;
+}
+
+function styleClassContainer(classContainer, data) {
+    classContainer.style.height = calculateHeight(data.start, data.end) + "px";
+    classContainer.style.top = calculateTop(data.start) + "px";
 }
 
 function createClassContainer(data) {
@@ -14,25 +52,34 @@ function createClassContainer(data) {
     var subject = createAbbreviation(data.subject);
 
     classContainer.innerHTML = `
-        <div class="start">
-            <h1>${data.start}</h1>
+        <div class="time">
+            ${data.start + "-" + data.end}
         </div>
         <div class="subject">
-            <h1>${subject}</h1>
+            ${subject}
         </div>
         <div class="teacher">
-            <h1>${data.start}</h1>
+            ${data.teacher}
         </div>
     `;
+    // style the class container
+    styleClassContainer(classContainer, data);
     document.body.appendChild(classContainer);
 }
 
-function loadjson() {
-    fetch("localhost:9000/test.json").then(response => {
-        console.log(response);
-    }).catch(error => {
-        console.log(error);
-    });
+function createWeeks(data) {
+    let keys = Object.keys(data);
+    let firstDay = keys[0];
+    let dateParts = firstDay.split("/");
+    let firstDate = new Date(+dateParts[2], dateParts[1] - 1, +dateParts[0]);
+    let firstDayOfWeek = firstDate.getDay();
+    
+    let lastDay = keys[keys.length - 1];
+    dateParts = lastDay.split("/");
+    let lastDate = new Date(+dateParts[2], dateParts[1] - 1, +dateParts[0]);
+    let lastDayOfWeek = lastDate.getDay();
+
+    let weeks = Math.ceil((lastDate - firstDate) / (7 * 24 * 60 * 60 * 1000));
 }
 
 loadjson();
