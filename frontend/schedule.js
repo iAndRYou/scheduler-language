5ebd29b1b2ff3f39b223e91ad2f2215eeb279fe7
iAndRYou <nawet3razy@gmail.com>
2023-06-20 14:56:01 +0200

    weeks

--- frontend/schedule.js
+++ frontend/schedule.js
@@ -1,16 +1,12 @@
-function loadjson() {
-    // output.json
-    fetch("http://[::]:9090/output.json").then(response =>
-        response.json().then(data => {
-            console.log(data);
-            for (var key in data) {
-                console.log(data[key]);
-                createClassContainer(data[key]);
-            }
-        }
-    ).catch(err => {
+async function loadjson() {
+    try {
+        // get the json file from the local server at port 9090
+        const response = await fetch("http://[::]:9090/output.json");
+        const data = await response.json();
+        return data;
+    } catch (err) {
         console.log(err);
-    }));
+    }
 }
 
 function createAbbreviation(subject) {
@@ -67,19 +63,47 @@ function createClassContainer(data) {
     document.body.appendChild(classContainer);
 }
 
-function createWeeks(data) {
-    let keys = Object.keys(data);
-    let firstDay = keys[0];
-    let dateParts = firstDay.split("/");
-    let firstDate = new Date(+dateParts[2], dateParts[1] - 1, +dateParts[0]);
-    let firstDayOfWeek = firstDate.getDay();
+function parseDate(dateString) {
+    const parts = dateString.split("/");
+    const day = parseInt(parts[0], 10);
+    const month = parseInt(parts[1], 10) - 1;
+    const year = parseInt(parts[2], 10);
     
-    let lastDay = keys[keys.length - 1];
-    dateParts = lastDay.split("/");
-    let lastDate = new Date(+dateParts[2], dateParts[1] - 1, +dateParts[0]);
-    let lastDayOfWeek = lastDate.getDay();
-
-    let weeks = Math.ceil((lastDate - firstDate) / (7 * 24 * 60 * 60 * 1000));
+    return new Date(year, month, day);
 }
 
-loadjson();
+function dateToString(date) {
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+
+    return `${day}/${month}/${year}`;
+}
+
+function calculateCalendarWeeks(data) {
+    const dates = Object.keys(data);
+    const startDate = parseDate(dates[0]);
+    const endDate = parseDate(dates[dates.length - 1]);
+  
+    const millisecondsPerDay = 24 * 60 * 60 * 1000;
+    const startDayOfWeek = (startDate.getDay() + 6) % 7; 
+    const endDayOfWeek = (endDate.getDay() + 6) % 7; 
+  
+    const daysInSpan = Math.ceil((endDate - startDate) / millisecondsPerDay) + 1; 
+    const totalDaysInWeeks = daysInSpan + startDayOfWeek + (6 - endDayOfWeek);
+    const numOfWeeks = Math.floor(totalDaysInWeeks / 7); 
+  
+    const adjustedStartDate = new Date(startDate.getTime() - startDayOfWeek * millisecondsPerDay);
+  
+    return { "startDate": adjustedStartDate, "numOfWeeks": numOfWeeks };
+}
+
+function createWeeks(data) {
+    let weeks = calculateCalendarWeeks(data);
+    console.log(weeks);
+}
+
+loadjson().then(data => {
+    console.log(data);
+    createWeeks(data);
+});
